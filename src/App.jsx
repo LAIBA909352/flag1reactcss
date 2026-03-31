e9eafcacb70b3489f87666dea08288e0aebbdb84
LAIBA909352 <sportstorm4@gmail.com>
2026-03-31 06:31:21 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="main">
+        <div className="a"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+      </div>
+    </>
+  );
+}
